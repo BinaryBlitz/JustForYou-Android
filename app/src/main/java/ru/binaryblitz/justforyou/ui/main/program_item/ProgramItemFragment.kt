2f3adb8d736662fb91bf06c7f09f692414a65819
Ilia Savin <savinilya34@gmail.com>
2017-08-24 17:11:44 +0300

    Add order screen template

--- app/src/main/java/ru/binaryblitz/justforyou/ui/main/program_item/ProgramItemFragment.kt
+++ app/src/main/java/ru/binaryblitz/justforyou/ui/main/program_item/ProgramItemFragment.kt
@@ -6,28 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_detailed_program.numberPicker
 import kotlinx.android.synthetic.main.activity_detailed_program.proceedProgramButton
-import kotlinx.android.synthetic.main.fragment_program_item.addToCartButton
-import kotlinx.android.synthetic.main.fragment_program_item.closeAlert
-import kotlinx.android.synthetic.main.fragment_program_item.dayPickerAlertSheet
 import kotlinx.android.synthetic.main.fragment_program_item.programContainerView
 import kotlinx.android.synthetic.main.fragment_program_item.programDescription
 import kotlinx.android.synthetic.main.fragment_program_item.programImage
-import kotlinx.android.synthetic.main.fragment_program_item.programItemContainer
 import kotlinx.android.synthetic.main.fragment_program_item.programPricePerDay
 import kotlinx.android.synthetic.main.fragment_program_item.programPricePerWeek
 import kotlinx.android.synthetic.main.fragment_program_item.programTitle
 import ru.binaryblitz.justforyou.R
-import ru.binaryblitz.justforyou.components.Constants
-import ru.binaryblitz.justforyou.data.cart.CartLocalStorage
-import ru.binaryblitz.justforyou.data.cart.ProgramsStorage
 import ru.binaryblitz.justforyou.data.programs.Program
 import ru.binaryblitz.justforyou.ui.router.Router
 
 class ProgramItemFragment : MvpAppCompatFragment() {
-  var cartProgramsLocalStorage: CartLocalStorage = ProgramsStorage()
-
   override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
       savedInstanceState: Bundle?): View? {
     return inflater?.inflate(R.layout.fragment_program_item, container, false)
@@ -42,27 +32,12 @@ class ProgramItemFragment : MvpAppCompatFragment() {
     programDescription.text = program.description
     programPricePerDay.text = getString(R.string.per_one_day) + "${program.primaryPrice}"
     programPricePerWeek.text = getString(R.string.per_ten_days) + "${program.secondaryPrice}"
-    programContainerView.setOnClickListener { Router.openDetailedProgramScreen(activity, program) }
-    proceedProgramButton.setOnClickListener { showDayPickerAlert() }
-    closeAlert.setOnClickListener { hideDayPickerAlert() }
-
-    numberPicker.maxValue = Constants.MAX_PROGRAM_DAYS
-    numberPicker.minValue = Constants.MIN_PROGRAM_DAYS
-
-    addToCartButton.setOnClickListener {
-      cartProgramPresenter.addProgramToCart(program, programItemContainer, cartProgramsLocalStorage,
-          numberPicker)
-      hideDayPickerAlert()
+    programContainerView.setOnClickListener {
+      Router.openDetailedProgramScreen(activity, program, ProgramsActivity.blockName)
     }
-  }
-
-
-  fun showDayPickerAlert() {
-    dayPickerAlertSheet.visibility = View.VISIBLE
-  }
-
-  fun hideDayPickerAlert() {
-    dayPickerAlertSheet.visibility = View.GONE
+    proceedProgramButton.setOnClickListener {
+      Router.openOrderScreen(activity, program, ProgramsActivity.blockName)
+    }
   }
 
   companion object {
