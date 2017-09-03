da5cbc625855fa3a50dc2c2e289fc6c974637345
Ilia Savin <savinilya34@gmail.com>
2017-09-03 07:56:39 +0300

    Implement support screen

--- app/src/main/java/ru/binaryblitz/justforyou/ui/main/support/SupportFragment.kt
+++ app/src/main/java/ru/binaryblitz/justforyou/ui/main/support/SupportFragment.kt
@@ -1,13 +1,23 @@
 package ru.binaryblitz.justforyou.ui.main.support
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.arellomobile.mvp.MvpAppCompatFragment
+import kotlinx.android.synthetic.main.fragment_support.callSupport
+import kotlinx.android.synthetic.main.fragment_support.facebook
+import kotlinx.android.synthetic.main.fragment_support.instagram
+import kotlinx.android.synthetic.main.fragment_support.sendSupport
 import ru.binaryblitz.justforyou.R
+import ru.binaryblitz.justforyou.ui.router.Router
+
 
 class SupportFragment : MvpAppCompatFragment() {
+  val supportNumber = "+7(495) 241 49 63"
+  val email = "info@justforyou.ru"
 
   override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
       savedInstanceState: Bundle?): View? {
@@ -16,5 +26,29 @@ class SupportFragment : MvpAppCompatFragment() {
 
   override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    callSupport.setOnClickListener { dialPhoneNumber(supportNumber) }
+    sendSupport.setOnClickListener {
+      sendEmail(email)
+    }
+    instagram.setOnClickListener {
+      Router.openJustForYouLink(activity, "https://www.instagram.com/justforyou_ru/")
+    }
+    facebook.setOnClickListener {
+      Router.openJustForYouLink(activity, "https://www.facebook.com/JUSTFORYOURUSSIA")
+    }
+  }
+
+  fun dialPhoneNumber(phoneNumber: String) {
+    val intent = Intent(Intent.ACTION_DIAL)
+    intent.data = Uri.parse("tel:" + phoneNumber)
+    if (intent.resolveActivity(activity.getPackageManager()) != null) {
+      startActivity(intent)
+    }
+  }
+
+  fun sendEmail(email: String) {
+    val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+        "mailto", email, null))
+    startActivity(Intent.createChooser(emailIntent, "Отправка сообщения менеджеру..."))
   }
 }
